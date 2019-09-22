40a37eb96416262b6d7cbe705d2666d349b3bfe6
Ahmed ElHor <zizooo.elhor@gmail.com>
2019-09-22 13:13:52 +0200

    initail

---
+++ Pages/Language.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestLocalization.Pages
+{
+    public class LanguageModel : PageModel
+    {
+        public RedirectResult OnGet(string culture, string returnUrl)
+        {
+            HttpContext.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture == "en" ? "ar" : "en")),
+                // making cookie valid for the actual app root path (which is not necessarily "/" e.g. if we're behind a reverse proxy)
+                new CookieOptions { Path = Url.Content("~/") });
+
+            return Redirect(returnUrl);
+        }
+    }
+}
